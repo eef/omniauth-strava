471df7f8195dc907dde659ba7a6f79e77a788696
Arthur Canal <arthur.canal@googlemail.com>
2013-03-17 01:59:13 +0000

    updated the domain

--- lib/omniauth/strategies/strava.rb
+++ lib/omniauth/strategies/strava.rb
@@ -4,6 +4,19 @@ module OmniAuth
   module Strategies
     class Strava < OmniAuth::Strategies::OAuth2
 
+      option :client_options, {
+        :site          => 'https://www.strava.com'
+      }
+
+      # uid{ raw_info['id'] || verified_email }
+
+      info do
+        puts raw_info
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('https://www.googleapis.com/oauth2/v1/userinfo').parsed
+      end
 
     end
   end
