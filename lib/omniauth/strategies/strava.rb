62b18017d2123b58802b21d624ab23cade22e40f
Arthur Canal <arthur.canal@googlemail.com>
2013-03-17 02:27:11 +0000

    forcing prompt

--- lib/omniauth/strategies/strava.rb
+++ lib/omniauth/strategies/strava.rb
@@ -11,15 +11,15 @@ module OmniAuth
 
       # uid{ raw_info['id'] || verified_email }
 
-      info do
-        puts "*"*100
-        ap access_token
-        puts raw_info
-      end
+      # info do
+      #   puts "*"*100
+      #   ap access_token
+      #   puts raw_info
+      # end
 
-      def raw_info
-        @raw_info ||= access_token.get('https://www.strava.com/api/v3/athlete').parsed
-      end
+      # def raw_info
+      #   @raw_info ||= access_token.get('https://www.strava.com/api/v3/athlete').parsed
+      # end
 
     end
   end
